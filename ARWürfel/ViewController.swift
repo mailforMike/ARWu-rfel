2a0d1688f7390c601f7a420e487a7c0bec6bba9d
michael <michael@mholzinger.at>
2018-11-03 19:00:34 +0100

    version 1 von kurs

--- ARWürfel/ViewController.swift
+++ ARWürfel/ViewController.swift
@@ -14,8 +14,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var würfelArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       // sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+        
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -23,11 +31,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +39,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
+        
+        //configuration.environmentTexturing = .automatic
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -47,34 +54,93 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    @IBAction func rollButton(_ sender: UIBarButtonItem) {
         
+        rollAll()
+    
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first{
+                
+                let würfelszene = SCNScene(named: "art.scnassets/würfel/diceCollada.scn")
+                
+                let würfel = würfelszene?.rootNode.childNode(withName: "Dice", recursively: true)
+                würfel?.position = SCNVector3(
+                    x: hitResult.worldTransform.columns.3.x,
+                    y: hitResult.worldTransform.columns.3.y + (würfel?.boundingSphere.radius)!,
+                    z: hitResult.worldTransform.columns.3.z
+                )
+                
+                würfelArray.append(würfel!)
+               
+                sceneView.scene.rootNode.addChildNode(würfel!)
+                
+                roll(würfel: würfel!)
+                
+            }
+            
+        }
     }
+    
+    func rollAll(){
+        if !würfelArray.isEmpty {
+            for würfel in würfelArray {
+                roll(würfel: würfel)
+            }
+        }
+    }
+    
+    func roll(würfel: SCNNode){
+        
+        let randomX = Float(arc4random_uniform(4)+1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4)+1) * (Float.pi/2)
+        
+        würfel.runAction(SCNAction.rotateBy(
+            x: CGFloat(randomX) * 3,
+            y: 0,
+            z: CGFloat(randomZ) * 3,
+            duration: 2))
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            print("plane detected")
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(planeAnchor.center.x,0,planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0) //rotation 90° x achse
+            
+            let material = SCNMaterial()
+            
+            material.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [material]
+            
+            planeNode.geometry = plane
+            
+            //node.addChildNode(planeNode) // <-- anchor planes anzeigen
+            
+            
+        } else {
+            return
+        }
+    }
+    
+    
+    
 }
